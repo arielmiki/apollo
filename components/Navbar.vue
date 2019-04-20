b86e7d489e8c50e176ebb874f24e660bc50bb62b
arielmiki <arielabraham82@gmail.com>
2019-04-20 21:08:36 +0700

    Add navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+    v-toolbar(color='transparent').elevation-0
+        v-spacer
+        v-toolbar-items
+            v-hover
+                v-btn(flat icon slot-scope='{ hover}'  @click="$store.commit('theme/toggle')")
+                    v-icon(:color='hover?"secondary":""') {{isDark?'brightness_2':'wb_sunny'}}
+            v-btn(flat) Home
+            v-btn(flat) Experience
+            v-btn(flat) Projects
+            v-btn(flat) Skills
+            v-btn(color='secondary' href='https://blog.arielmiki.xyz' target='_blank') Blog
+</template>
+
+<script>
+export default {
+  name: 'navbar',
+  computed: {
+    isDark: {
+      set: function(value) {
+          this.$store.commit('theme/toggle');
+      },
+      get: function() {
+        return this.$store.state.theme.isDark;
+      }
+    }
+  }
+}
+</script>
---
+++ store/theme.js
@@ -0,0 +1,9 @@
+export const state = () => ({
+    isDark: false
+  })
+  
+export const mutations = {
+    toggle (state) {
+      state.isDark = !state.isDark
+    }
+}
