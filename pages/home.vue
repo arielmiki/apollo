d93afb4e88724df9b4688d0d914e83e87b5e362d
arielmiki <arielabraham82@gmail.com>
2019-04-21 21:21:31 +0700

    Change favicon and background color

--- pages/home.vue
+++ pages/home.vue
@@ -10,7 +10,7 @@
             . 
               . Currently, I am a final-year computer science student at <strong class='font-weight-bold'>University of Indonesia</strong>.
               My interest varies from  <strong>software development</strong>, <strong>data science</strong>,
-              <strong>embedded systems</strong>, <strong>physics</strong>, and <strong>bioinformatics</strong>.
+              <strong>embedded systems</strong>, <strong>particle physics</strong>, and <strong>bioinformatics</strong>.
               I spent my leasure time for playing game, <strong><u>trying</u></strong> to play music, and reading books.
             
          
@@ -18,6 +18,12 @@
 <script>
 export default {}
 </script>
-
-
+<style scoped>
+.display-4{
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-select: none;
+}
+</style>
 
