c13102ae81900d615a0f5623937140e321590385
arielmiki <arielabraham82@gmail.com>
2019-06-08 15:20:00 +0700

    Change lading pages layout

--- components/home/Content.vue
+++ components/home/Content.vue
@@ -7,8 +7,8 @@
             p.subheading.font-weight-thin
               | My name is 
               strong.font-weight-bold.accent--text Ariel Miki Abraham
-              . 
-                . Currently, I am a final-year computer science student at <strong class='font-weight-bold'>University of Indonesia</strong>.
+              .
+                . Currently, I am a final-year computer science student at <a class="info--text" href="https://cs.ui.ac.id"><strong class='font-weight-bold'>University of Indonesia</strong></a>.
                 My interest varies from  <strong>software development</strong>, <strong>data science</strong>,
                 <strong>embedded systems</strong>, <strong>particle physics</strong>, and <strong>bioinformatics</strong>.
                 I spent my leasure time for playing game, <strong><u>trying</u></strong> to play music, and reading books.
